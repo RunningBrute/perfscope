af026021740185abb1152e95b2ec34c5f530769b
RunningBrute <piotrekbas8@gmail.com>
2026-04-06 21:59:48 +0200

    add test for vector with reserve

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -13,6 +13,20 @@ static void BM_VectorPushBack(benchmark::State& state)
     }
 }
 
+static void BM_VectorPushBackWithReserve(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        std::vector<int> v;
+        v.reserve(1000);
+        for (int i = 0; i < 1000; ++i)
+        {
+            v.push_back(i);
+        }
+    }
+}
+
 BENCHMARK(BM_VectorPushBack);
+BENCHMARK(BM_VectorPushBackWithReserve);
 
 BENCHMARK_MAIN();
